4e666901c6dae395276a34aa67beba5e2b74f418
Matheus da Silva Ferreira de Azevedo <matheus.sfazevedo@gmail.com>
2024-06-16 22:23:14 -0300

    RenancoD

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('imagem').src = "paisagem.jpg";
+
+document.getElementById('link1').textContent = "http://www.google.com/";
+
+document.getElementById('paragrafo').style.color = "red";
+
+document.location.href('http://www.exemple.com/').textContent = "http://www.google.com/";
